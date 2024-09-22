3cde3819510941d12e899faebae2d1efd0add2b0
孑愁 <401828628@qq.com>
2024-09-22 09:57:08 +0800

    完善环境

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // 箭头函数，只有一个参数的时候，不需要括号
+  arrowParens: 'avoid',
+  // jsx 标签的反尖括号需要换行
+  bracketSameLine: true,
+  // 大括号内的首尾需要空格
+  bracketSpacing: false,
+  // 使用单引号
+  singleQuote: true,
+  // 末尾不需要逗号 'es5'  none
+  trailingComma: 'all',
+  //不检测前后空格格式
+  endOfLine: 'auto',
+};
